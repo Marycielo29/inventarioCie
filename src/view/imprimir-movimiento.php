3cf70ad2f5126f652f49c5be689e5276c3f7c528
Marycielo29 <marycielomb29@gmail.com>
2025-06-24 12:56:48 -0500

    24/06

--- src/view/imprimir-movimiento.php
+++ src/view/imprimir-movimiento.php
@@ -29,7 +29,7 @@ $curl = curl_init(); //inicia la sesión cURL
     if ($err) {
         echo "cURL Error #:" . $err; // mostramos el error
     } else {
-        echo $response; // en caso de funcionar correctamente
-       /* echo $_SESSION['sesion_sigi_id'];
-        echo $_SESSION['sesion_sigi_token'];*/
+        $respuesta = json_decode($response);
+        print_r($respuesta);
+        
     }
